a989b5767eb19fbe7c7ff721fb72d4c8336fdd87
Venkata Phani Bhaskar Sarma Mantrala <venkata.mantrala@oportun.com>
2022-02-01 16:30:34 -0800

    Removed RestController from Main App

--- src/main/java/com/bhaskar/springbootjava17/SpringBootJava17Application.java
+++ src/main/java/com/bhaskar/springbootjava17/SpringBootJava17Application.java
@@ -6,17 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringBootJava17Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJava17Application.class, args);
-    }
-
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
     }
 
 }
